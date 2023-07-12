a90e4462d979eb9e45ee3c8d4e796590d321536e
Hakkı Mert Dirgar <123075895+dirgarmert@users.noreply.github.com>
2023-07-12 22:53:14 +0300

    Add files via upload
    -dart pages

---
+++ constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// ignore: constant_identifier_names
+const String google_api_key = "AIzaSyAoifbCMWUPLqPPZGA1k8ud2YP1hefd2kc";
+const Color primaryColor = Color.fromARGB(0, 24, 165, 201);
+const double defaultPadding = 16.0;
---
+++ styles.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Styles {
+    static const appBlue = Color(0xff0099cc);
+    static const appOrange = Color.fromARGB(255, 255, 5, 5); 
+
+
+
+}
